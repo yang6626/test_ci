13c0f2f3e1cd46e96deb81c5ef6ff4eed9b638ac
yang6626 <305020042@qq.com>
2022-01-08 17:14:00 +0800

    1

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@
  * @Author: yang yu
  * @Date: 2022-01-08 16:39:31
  * @LastEditors: yang yu
- * @LastEditTime: 2022-01-08 16:39:33
+ * @LastEditTime: 2022-01-08 17:13:48
  * @Description: file content
  */
 const express = require("express");
 const app = express();
 
 app.get("/test", (_req, res) => {
-  res.status(200).send("Hello world");
+  res.status(200).send("Hello world!");
 });
 module.exports = app;
